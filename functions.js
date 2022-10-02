a6c6c2ed6eaac598d995f14d0543617127a99472
Sarah Rodriguez <sarahesther00@yahoo.com>
2022-10-01 19:26:57 -0500

    JS functions to compute volume

---
+++ functions.js
@@ -0,0 +1,14 @@
+function doInputOutput()
+    function houseVolume(width, depth, height, sweep) {
+        livingVolume + roofVolume
+    }
+    function livingVolume(width, depth, height) {
+        width * depth * height
+    }
+    function roofVolume(width, depth, sweep) {
+        triangleArea * width
+    }
+    function triangleArea(a, b, c) {
+        let s = a + b + c / 2
+        Math.sqrt(s(s-a)(s-b)(s-c))
+    }
