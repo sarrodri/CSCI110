a78f18751e61bb1581fc7106956517fb7ed2b093
Sarah Rodriguez <sarahesther00@yahoo.com>
2022-10-06 11:24:43 -0500

    Done!

--- houseVolumeProgram.js
+++ houseVolumeProgram.js
@@ -1,31 +1,26 @@
 // JS functions to compute volume
+var height
+var width
+var depth
+var sweep
 
-function doInputOutput(){    
-    
-    let height=document.getElementById("heightInput").value;
-    let width=document.getElementById("widthInput").value;
-    let depth=document.getElementById("depthInput").value;
-    let sweep=document.getElementById("sweepInput").value;
-    let housevol = houseVolume(width, depth, height, sweep)
-
-    document.getElementById("outputDiv").innerHTML = housevol;
+function doInputOutput() {
+    height = parseInt(document.getElementById("heightInput").value)
+    width = parseInt(document.getElementById("widthInput").value)
+    depth = parseInt(document.getElementById("depthInput").value)
+    sweep = parseInt(document.getElementById("sweepInput").value)
+    let housevol = houseVolume()
+    document.getElementById("outputDiv").innerHTML = housevol
 }
-
-    function houseVolume(width, depth, height, sweep) {
-        let space = livingVolume(width, depth, height);
-        let roof = roofVolume(width,depth,sweep);
-        return space + roof;
-    }
-    function livingVolume(width, depth, height) {
-        livingSpace = width * depth * height;
-        return livingSpace;
-    }
-    function roofVolume(width, depth, sweep) {
-        triArea = triangleArea(sweep, sweep, depth);
-        return triArea * width;
-    }
-    function triangleArea(sweep, sweep, depth) {
-        let s = (sweep + sweep + depth) / 2;
-        let area = Math.sqrt(s*(s-sweep)*(s-sweep)*(s-depth));
-        return area;
-    } 
+function houseVolume() {
+    return livingVolume() + roofVolume()
+}
+function livingVolume() {
+    return width * depth * height
+}
+function roofVolume() {
+    return triangleArea() * width
+}
+function triangleArea() {
+    return (height + width + sweep) / 2
+}
